b14cb696bd458d0841b86c15b90d46346b3a2800
Andre <Torres>
2020-07-27 18:07:58 -0400

    corinthia render2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+
+
+export const App = () => (
+    
+        <div className="App">
+            <header className="App-header">
+                <p>Vai curinthia!</p>
+            </header>
+        </div>
+);
+
