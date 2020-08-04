25e33cc79442fb8effc27fcb0f098138cd94d3b3
Andre <Torres>
2020-08-04 19:53:21 -0400

    footer commit

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
 import { Tasks } from '../Tasks';
+import { Footer } from './Footer';
 
 export const Content = () => (
   <section className="content">
     <Sidebar />
     <Tasks />
+    <Footer />
   </section>
 );
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import { FaRegMoon } from 'react-icons/fa';
-import PropTypes from 'prop-types';
+import { FaCopyright } from 'react-icons/fa';
+
 
 
 
@@ -10,13 +10,9 @@ export const Footer = () => {
     return (
         <div className="footer" data-testid="footer">
             <h4>
-                ANDRE TORRES © 2020 
+                ANDRE TORRES <FaCopyright /> 2020
             </h4>
         </div>
     )
 
-};
-Footer.propTypes = {
-    darkMode: PropTypes.bool.isRequired,
-    setDarkMode: PropTypes.func.isRequired,
 };
